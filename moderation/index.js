7b0f138f6ed11948e8b9b571e0f100364c67f040
Mehmet <mathdenizi@gmail.com>
2024-09-04 02:12:09 +0200

    moderate service created

---
+++ moderation/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentCreated") {
+    const updatedStatus = data.content.includes("orange")
+      ? "rejected"
+      : "approved";
+  }
+
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentModerated",
+    data: {
+      id: data.id,
+      content: data.contend,
+      status: updatedStatus,
+      postId: data.postId,
+    },
+  });
+
+  res.send({ message: "Event received." });
+});
+
+app.listen(4003, () => {
+  console.log("Listening on 4003");
+});
